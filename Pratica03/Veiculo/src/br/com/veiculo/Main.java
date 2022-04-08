978e7ea5c730953f11a506165fafa51a35cbfc42
amiltonbhz <71555277+amiltonbhz@users.noreply.github.com>
2022-04-08 09:28:39 -0300

    Add files via upload

---
+++ Pratica03/Veiculo/src/br/com/veiculo/Main.java
@@ -0,0 +1,13 @@
+package br.com.veiculo;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Veiculo carro = new Veiculo(null, 0);
+		
+		//implementar exibir dados em veículo usando javaX
+		
+
+	}
+
+}
